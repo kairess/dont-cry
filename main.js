5017be557312dcc8cc9c1c8897d32a63bcb35ed0
Brad <kairess87@gmail.com>
2023-11-09 15:54:09 +0900

    refactory

--- main.js
+++ main.js
@@ -13,6 +13,30 @@ const app = document.getElementById('app');
 
 const drawSkeleton = (sketch) => {
   for (let i = 0; i < poses.length; i++) {
+    // Keypoints
+    for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
+      let keypoint = poses[i].pose.keypoints[j];
+
+      if (keypoint.score > 0.5) {
+        sketch.fill(255, 255, 0);
+        sketch.ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+
+    // Skeleton
+    let skeleton = poses[i].skeleton;
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+
+      sketch.stroke(255, 255, 0);
+      sketch.line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+};
+
+const drawDrops = (sketch) => {
+  for (let i = 0; i < poses.length; i++) {
     let leftEye = poses[i].pose.leftEye;
     let rightEye = poses[i].pose.rightEye;
 
@@ -23,43 +47,22 @@ const drawSkeleton = (sketch) => {
     if (rightEye.confidence > 0.9)
       drops.push(new Drop(rightEye.x, rightEye.y, 5, sketch))
     }
-
-    // // Keypoints
-    // for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
-    //   let keypoint = poses[i].pose.keypoints[j];
-
-    //   if (keypoint.score > 0.5) {
-    //     sketch.fill(255, 255, 0);
-    //     sketch.ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-    //   }
-    // }
-
-    // // Skeleton
-    // let skeleton = poses[i].skeleton;
-    // for (let j = 0; j < skeleton.length; j++) {
-    //   let partA = skeleton[j][0];
-    //   let partB = skeleton[j][1];
-
-    //   sketch.stroke(255, 255, 0);
-    //   sketch.line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-    // }
   }
-}
+};
 
 class Drop {
   constructor(x, y, d, sketch) {
+    this.d = d;
+    this.sketch = sketch;
+
     this.body = Bodies.circle(x, y, d, {
       restitution: 0.2,
     });
-    this.d = d;
-    this.sketch = sketch;
 
     World.add(world, this.body);
   }
 
   show() {
-    this.sketch.push();
-
     this.sketch.fill(29, 168, 242, 150);
     this.sketch.stroke(29, 168, 242, 150);
     this.sketch.ellipse(this.body.position.x, this.body.position.y + this.d * 3, this.d * 2, this.d * 2);
@@ -74,10 +77,8 @@ class Drop {
     // this.sketch.fill(29, 168, 242);
     // this.sketch.noStroke();
     // this.sketch.circle(this.body.position.x, this.body.position.y, this.d * 2);
-    
-    this.sketch.pop();
   }
-}
+};
 
 new p5((sketch) => {
   let video;
@@ -98,7 +99,8 @@ new p5((sketch) => {
 
   sketch.draw = () => {
     sketch.image(video, 0, 0, 640, 480);
-    drawSkeleton(sketch);
+    // drawSkeleton(sketch);
+    drawDrops(sketch);
 
     for (var i = 0; i < drops.length; i++) {
       drops[i].show();
